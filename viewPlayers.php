26038efb6a32392f9ff87e8113d59c36c68de818
wangeshiii <100954600+wangeshiii@users.noreply.github.com>
2022-03-10 10:06:12 +0300

    Added the view players and view users files

---
+++ viewPlayers.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("connect.php");
+    require_once("processLogin.php");
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PLAYERS</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/view.css">
+    </head>
+    <body>
+        <div class="bgImage">
+            <div class="container">
+                <div id="topNavBar">
+                    <ul>
+                        <li class="title"><a href="viewPlayers.php">STIMA CLUB</a></li>
+                        <li><a href="admin.php">HOME</a></li>
+                        <li><a href="addPlayer.php">ADD PLAYER</a></li>
+                    </ul>
+                </div>
+            </div> 
+                
+        </div>
+    </body>
+</html>
